3eaa1c49e39dd824a5770954d8017662d2d90309
alealcruz <alealcruz@gmail.com>
2021-09-21 13:58:41 +0200

    Saludo ampliado

--- holaMundo.java
+++ holaMundo.java
@@ -4,5 +4,6 @@ Muestra por pantalla el mensaje "Hola Mundo"
 public class holaMundo {
   public static void main(String[] args) {
     System.out.println("¡Hola mundo!");
+    System.out.println("Aquí estamos haciendo pruebas con Git.");
   }
 }
